2102f355a4e169c6cc3fa2dbad6b224c978238d4
Crim <stephen.powis@gmail.com>
2017-10-14 13:02:53 +0900

    Setup UI with time picker

--- kafka-webview/src/main/resources/static/js/app.js
+++ kafka-webview/src/main/resources/static/js/app.js
@@ -161,6 +161,14 @@ var ApiClient = {
   consumeHead: function(viewId, callback) {
       ApiClient.consume(viewId, {action:'head'}, callback);
   },
+  seekTimestamp: function(viewId, unixTimestamp, callback) {
+      jQuery.ajax({
+          type: 'POST',
+          url: '/api/consumer/view/' + viewId + '/timestamp/' + unixTimestamp,
+          dataType: 'json',
+          success: callback,
+      });
+  },
   setConsumerState: function(viewId, partitionOffsetJson, callback) {
       //jQuery.post('/api/consumer/view/' + viewId + '/offsets', partitionOffsetMap, callback);
       jQuery.ajax({
@@ -190,7 +198,49 @@ var DateTools = {
   localTimezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
   displayTimestamp: function(timestampMs) {
     // Adjusts timestamp into local timezone and locate
-    return new Date(timestampMs).toLocaleString();
+    //return new Date(timestampMs).toLocaleString();
+      var myDate = new Date(timestampMs);
+
+      var year = myDate.getFullYear();
+      var month = myDate.getMonth() + 1;
+      var day = myDate.getDate();
+      var hour = myDate.getHours();
+      var min = myDate.getMinutes();
+      var sec = myDate.getSeconds();
+
+      month = (month < 10 ? "0" : "") + month;
+      day = (day < 10 ? "0" : "") + day;
+      hour = (hour < 10 ? "0" : "") + hour;
+      min = (min < 10 ? "0" : "") + min;
+      sec = (sec < 10 ? "0" : "") + sec;
+
+      return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec + DateTools.formatTz(myDate);
+  },
+  formatTz: function(date) {
+      var timezone_offset_min = date.getTimezoneOffset(),
+          offset_hrs = parseInt(Math.abs(timezone_offset_min/60)),
+          offset_min = Math.abs(timezone_offset_min%60),
+          timezone_standard;
+
+      if (offset_hrs < 10) {
+          offset_hrs = '0' + offset_hrs;
+      }
+
+      if (offset_min < 10) {
+          offset_min = '0' + offset_min;
+      }
+
+      // Add an opposite sign to the offset
+      // If offset is 0, it means timezone is UTC
+      if(timezone_offset_min < 0) {
+          timezone_standard = '+' + offset_hrs + ':' + offset_min;
+      } else if(timezone_offset_min > 0) {
+          timezone_standard = '-' + offset_hrs + ':' + offset_min;
+      } else if(timezone_offset_min == 0) {
+          timezone_standard = 'Z';
+      }
+
+      return timezone_standard;
   }
 };
 
