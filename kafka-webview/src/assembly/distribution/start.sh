e3f8051b469ba646684783681b88ff8d83fb7d62
Crim <stephen.powis@gmail.com>
2017-10-22 14:57:32 +0900

    build distro on package

---
+++ kafka-webview/src/assembly/distribution/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CWD=`pwd`
+
+## Change to local directory
+cd "${0%/*}"
+
+## Define configuration
+export SPRING_CONFIG_LOCATION=classpath:/config/base.yml,config.yml
+
+## launch webapp
+java -jar kafka-webview*.jar
+
+## Change back to previous directory
+cd $CWD
