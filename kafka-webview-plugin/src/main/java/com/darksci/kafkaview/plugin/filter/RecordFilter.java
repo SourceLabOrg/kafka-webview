b6d19a819e94f37f3352e996db497c3851e2599f
Crim <stephen.powis@gmail.com>
2017-10-23 20:21:32 +0900

    Start building out in-app help

--- kafka-webview-plugin/src/main/java/com/darksci/kafkaview/plugin/filter/RecordFilter.java
+++ kafka-webview-plugin/src/main/java/com/darksci/kafkaview/plugin/filter/RecordFilter.java
@@ -6,7 +6,6 @@ import java.util.Map;
  * Interface that defines a Record Filter.
  */
 public interface RecordFilter {
-    boolean filter(final String topic, final int partition, final long offset, final Object key, final Object value);
-
     void configure(final Map<String, ?> configs);
+    boolean filter(final String topic, final int partition, final long offset, final Object key, final Object value);
 }
--- kafka-webview/src/main/java/com/darksci/kafkaview/controller/home/HomeController.java
+++ kafka-webview/src/main/java/com/darksci/kafkaview/controller/home/HomeController.java
@@ -1,18 +1,42 @@
 package com.darksci.kafkaview.controller.home;
 
 import com.darksci.kafkaview.controller.BaseController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.darksci.kafkaview.repository.ClusterRepository;
+import com.darksci.kafkaview.repository.ViewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController extends BaseController {
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+    @Autowired
+    private ClusterRepository clusterRepository;
+
+    @Autowired
+    private ViewRepository viewRepository;
+
+    /**
+     * Once things are setup, we'll redirect the home page to view/index.
+     */
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home() {
+    public String home(final Model model) {
+        // Look for views
+        final long numberOfViews = viewRepository.count();
+
+        // If at least one view is configured
+        if (false && numberOfViews > 0) {
+            // Redirect to the vew index
+            return "redirect:/view";
+        }
+
+        // Look for clusters
+        final long numberOfClusters = clusterRepository.count();
+
+        model.addAttribute("hasView", (numberOfViews > 0));
+        model.addAttribute("hasCluster", (numberOfClusters > 0));
         return "home/index";
     }
 }
