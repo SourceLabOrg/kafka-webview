1fec2ca0e0452fc4fa1308a7a79bef1df2609777
Crim <stephen.powis@gmail.com>
2018-11-12 00:08:37 +0900

    Add test coverage to remove consumer operation

--- kafka-webview-ui/src/test/java/org/sourcelab/kafka/webview/ui/controller/api/ApiControllerTest.java
+++ kafka-webview-ui/src/test/java/org/sourcelab/kafka/webview/ui/controller/api/ApiControllerTest.java
@@ -25,6 +25,9 @@
 package org.sourcelab.kafka.webview.ui.controller.api;
 
 import com.salesforce.kafka.test.junit4.SharedKafkaTestResource;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,6 +42,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+import java.util.Properties;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.containsString;
@@ -47,6 +52,7 @@ import static org.junit.Assert.assertTrue;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -70,6 +76,7 @@ public class ApiControllerTest extends AbstractMvcTest {
     public void test_withoutAdminRole() throws Exception {
         testUrlWithOutAdminRole("/api/cluster/1/create/topic", true);
         testUrlWithOutAdminRole("/api/cluster/1/modify/topic", true);
+        testUrlWithOutAdminRole("/api/cluster/1/consumer/remove", true);
     }
 
     /**
@@ -177,5 +184,61 @@ public class ApiControllerTest extends AbstractMvcTest {
 
         assertTrue(resultJsonStr.contains(targetItem1));
         assertTrue(resultJsonStr.contains(targetItem2));
+    }
+
+    /**
+     * Test the list Consumers end point.
+     */
+    @Test
+    @Transactional
+    public void test_listConsumers() throws Exception {
+        final String consumerId = "test-consumer-id-" + System.currentTimeMillis();
+
+        // Create a cluster.
+        final Cluster cluster = clusterTestTools.createCluster(
+            "Test Cluster",
+            sharedKafkaTestResource.getKafkaConnectString()
+        );
+
+        // Define our new topic name
+        final String newTopic = "TestTopic-" + System.currentTimeMillis();
+        sharedKafkaTestResource
+            .getKafkaTestUtils()
+            .createTopic(newTopic, 1, (short)1);
+
+        // Publish records into topic
+        sharedKafkaTestResource
+            .getKafkaTestUtils()
+            .produceRecords(10, newTopic, 0);
+
+        // Create a consumer and consume from the records, maintaining state.
+        final Properties consumerProperties = new Properties();
+        consumerProperties.put("client.id", consumerId);
+        consumerProperties.put("group.id", consumerId);
+
+        try (final KafkaConsumer consumer = sharedKafkaTestResource
+            .getKafkaTestUtils()
+            .getKafkaConsumer(StringDeserializer.class, StringDeserializer.class, consumerProperties)) {
+
+            // Consume
+            consumer.subscribe(Collections.singleton(newTopic));
+            consumer.poll(2000L);
+
+            // Save state.
+            consumer.commitSync();
+        }
+        
+        // Hit end point
+        mockMvc
+            .perform(get("/api/cluster/" + cluster.getId() + "/consumers")
+                .with(user(nonAdminUserDetails))
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+
+            // Validate submit button seems to show up.
+            .andExpect(content().string(containsString("{\"id\":\"" + consumerId + "\",\"simple\":false}")));
     }
 }
