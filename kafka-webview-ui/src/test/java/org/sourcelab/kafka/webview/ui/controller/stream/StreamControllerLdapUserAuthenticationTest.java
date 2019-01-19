f1992d5007ff5c5d01e8e740ec0571ddb4943428
Steviep <Crim@users.noreply.github.com>
2019-01-19 10:04:50 +0900

    Update dependencies (#129)

--- kafka-webview-ui/src/test/java/org/sourcelab/kafka/webview/ui/controller/stream/StreamControllerLdapUserAuthenticationTest.java
+++ kafka-webview-ui/src/test/java/org/sourcelab/kafka/webview/ui/controller/stream/StreamControllerLdapUserAuthenticationTest.java
@@ -26,8 +26,6 @@ package org.sourcelab.kafka.webview.ui.controller.stream;
 
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zapodot.junit.ldap.EmbeddedLdapRule;
