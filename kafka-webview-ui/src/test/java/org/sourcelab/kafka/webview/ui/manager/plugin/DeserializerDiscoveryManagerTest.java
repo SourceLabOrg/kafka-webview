235998784c1bb2e04a09212d9a47dafdadc155a8
Stephen Powis <spowis@salesforce.com>
2019-06-19 15:57:35 +0900

    add migration

--- kafka-webview-ui/src/test/java/org/sourcelab/kafka/webview/ui/manager/plugin/DeserializerDiscoveryManagerTest.java
+++ kafka-webview-ui/src/test/java/org/sourcelab/kafka/webview/ui/manager/plugin/DeserializerDiscoveryManagerTest.java
@@ -33,8 +33,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DeserializerDiscoveryManagerTest {
