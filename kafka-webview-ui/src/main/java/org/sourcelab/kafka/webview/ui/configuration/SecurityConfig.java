e9a011bd010fc965a018c0085b35d608c4671c87
Matteo Baiguini <baiguini.matteo@gmail.com>
2019-12-19 09:28:23 +0100

    Kube-Series-1: Features/prometheus metrics (#198)
    * Added Micrometer Prometheus to Maven pom
    
    * Updated WebConfig to include a StringHttpMessageConverter in order to properly manage "Accept: text/plain" header in requests
    
    * Updated "dev" config to setup advanced Spring Actuator properties - Added WebServer config to allow secondary port on tomcat and fix Spring Actuator login page redirection failure
    
    * Updated README - Removed commented dev property
    
    * Added property to export all Tomcat metrics
    
    * Fixed Actuator login issue
    
    * Moved Actuator-related properties from dev.yml to base.yml
    
    * Added missing javadoc comment

--- kafka-webview-ui/src/main/java/org/sourcelab/kafka/webview/ui/configuration/SecurityConfig.java
+++ kafka-webview-ui/src/main/java/org/sourcelab/kafka/webview/ui/configuration/SecurityConfig.java
@@ -34,6 +34,7 @@ import org.sourcelab.kafka.webview.ui.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,6 +51,7 @@ import java.util.ArrayList;
  */
 @Configuration
 @EnableWebSecurity
+@Order(1001)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 
--- kafka-webview-ui/src/main/java/org/sourcelab/kafka/webview/ui/configuration/WebConfig.java
+++ kafka-webview-ui/src/main/java/org/sourcelab/kafka/webview/ui/configuration/WebConfig.java
@@ -31,6 +31,7 @@ import org.sourcelab.kafka.webview.ui.manager.jackson.SimpleAvroDataSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -84,6 +85,9 @@ public class WebConfig implements WebMvcConfigurer {
             .modulesToInstall(new ProtobufModule())
             .serializerByType(GenericData.Record.class, new SimpleAvroDataSerializer(appProperties.isAvroIncludeSchema()))
             .build();
+
         converters.add(new MappingJackson2HttpMessageConverter(mapper));
+        converters.add(new StringHttpMessageConverter());
     }
+
 }
