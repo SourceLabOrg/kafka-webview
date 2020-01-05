a43abe8228c7493926cfbd01fc16f2191e5561b5
Matteo Baiguini SBT <mbaiguini@swissblock.net>
2020-01-05 13:51:48 +0100

    Fix start script big mistake

--- kafka-webview-ui/src/assembly/distribution/start.sh
+++ kafka-webview-ui/src/assembly/distribution/start.sh
@@ -1,28 +1,35 @@
 #!/bin/sh
 
-# Temporary save current directory
-CWD=`pwd`
-
-# Change to local directory
-cd "${0%/*}"
+# Get the directory of this script
+SCRIPT_DIR=$(dirname "$0")
 
 # Define environment variables
 ## New
-export JVM_OPTS="-noverify -server -XX:TieredStopAtLevel=1"
-export MEM_OPTS="-Xms2G -Xmx2G -XX:MaxMetaspaceSize=300M"
-export JAVA_OPTS=""
+if [[ -z "$JVM_OPTS" ]]; then
+    export JVM_OPTS="-noverify -server -XX:TieredStopAtLevel=1"
+fi
+if [[ -z "$MEM_OPTS" ]]; then
+    export MEM_OPTS="-Xms2G -Xmx2G -XX:MaxMetaspaceSize=300M"
+fi
+if [[ -z "$JAVA_OPTS" ]]; then
+    export JAVA_OPTS=""
+fi
 ## Deprecated
 if [[ ! -z "$LOG_OPTS" ]]; then
     echo "Usage of 'LOG_OPTS' is deprecated and will be removed in the future, please switch to 'JAVA_OPTS'"
-    JAVA_OPTS="$JAVA_OPTS $LOG_OPTS"
+    export JAVA_OPTS="$JAVA_OPTS $LOG_OPTS"
 fi
 if [[ ! -z "$HEAP_OPTS" ]]; then
     echo "Usage of 'HEAP_OPTS' is deprecated and will be removed in the future, please switch to 'MEM_OPTS'"
-    export MEM_OPTS="$HEAP_OPTS"
+    export MEM_OPTS="$MEM_OPTS $HEAP_OPTS"
 fi
 
 # Start application
-exec java $JVM_OPTS $MEM_OPTS $JAVA_OPTS -jar kafka-webview-ui-*.jar
-
-# Change back to previous directory
-cd $CWD
+echo "Start Kafka-WebView with following parameters:"
+echo "\tJVM_OPTS: $JVM_OPTS"
+echo "\tMEM_OPTS: $MEM_OPTS"
+echo "\tJAVA_OPTS: $JAVA_OPTS"
+echo "\tLOG_OPTS: deprecated, added to JAVA_OPTS"
+echo "\tHEAP_OPTS: deprecated, added to MEM_OPTS"
+echo 
+java $JVM_OPTS $MEM_OPTS $JAVA_OPTS -jar kafka-webview-ui-*.jar
